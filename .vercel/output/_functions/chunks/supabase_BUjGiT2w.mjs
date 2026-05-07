db73381ae884da2eae7cfb4162278a2ac853a13d
Radványi Tamás <radvanyitamas@Radiir-MacBook-Pro.local>
2026-05-07 18:01:23 +0200

    fix

--- .vercel/output/_functions/chunks/supabase_BUjGiT2w.mjs
+++
@@ -1,7 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-const supabaseUrl = "https://sacqanudxgffzdwxtvri.supabase.co";
-const supabaseAnonKey = "sb_publishable_3xTq08uSCXmPGU1eEj2zlA_a4MBvxHP";
-const supabase = createClient(supabaseUrl, supabaseAnonKey);
-
-export { supabase as s };
--- .vercel/output/_functions/entry.mjs
+++
@@ -1,1 +0,0 @@
-export { x as default } from './chunks/entrypoint_Z1T7W8pZ.mjs';
