c392dbbdb79bb968ac511f400c32a46f091833b4
Radványi Tamás <radvanyitamas@Radiir-MacBook-Pro.local>
2026-05-07 19:45:00 +0200

    Add gallery CRUD

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = import.meta.env.SUPABASE_URL;
-const supabaseAnonKey = import.meta.env.SUPABASE_ANON_KEY;
-
+// A PUBLIC_ előtaggal a böngésző (kliens) is látni fogja ezeket
+const supabaseUrl = import.meta.env.PUBLIC_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.PUBLIC_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
