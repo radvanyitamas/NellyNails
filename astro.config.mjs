b3ca67514feaeb90fd633c3f1c7ad7a09c27a302
Radványi Tamás <radvanyitamas@Radiir-MacBook-Pro.local>
2026-05-07 18:06:33 +0200

    Fix: Explicitly ensure vercel adapter is in dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,12 +5,5 @@ import vercel from "@astrojs/vercel";
 export default defineConfig({
   integrations: [tailwind()],
   output: 'server',
-  adapter: vercel({
-    // Ez a sor kényszeríti a modernebb struktúrát
-    webAnalytics: { enabled: true }
-  }),
-  // Kényszerítsük a build kimenetet a gyökérbe
-  build: {
-    server: './dist/server/entry.mjs'
-  }
+  adapter: vercel()
 });
